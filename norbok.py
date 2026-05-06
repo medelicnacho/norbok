544cfda9e622e4f4887513094723575c2d71054a
medelicnacho <greencatluk@gmail.com>
2026-05-06 12:35:15 -0700

    feat: add norbok.py

---
+++ norbok.py
@@ -0,0 +1,10 @@
+import requests
+
+response = requests.post("http://localhost::11434/api/chat", json={
+    "model": "qwen2.5-coder:7b",
+    "messages": [{"role": "user", "content": "hello"}],
+    "stream": False
+    })
+
+data = response.json()
+print(data["message"]["content"])
